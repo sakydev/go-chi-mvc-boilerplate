a44f9655d246f686e1a4fe8b7a15204491e834eb
Saqib Razzaq <saqib.razzaq@plana.earth>
2024-04-20 12:29:05 +0200

    Add response errors

--- src/utils/response.go
+++ src/utils/response.go
@@ -5,8 +5,10 @@ import (
 	"net/http"
 )
 
+const defaultContentType = "application/json"
+
 func RenderResponse(w http.ResponseWriter, data interface{}, status int) {
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.Header().Set("Content-Type", defaultContentType)
 	w.WriteHeader(status)
 
 	jsonData, err := json.Marshal(data)
@@ -16,4 +18,12 @@ func RenderResponse(w http.ResponseWriter, data interface{}, status int) {
 	}
 
 	w.Write(jsonData)
+}
+
+func RenderNotFoundResponse(w http.ResponseWriter) {
+	RenderResponse(w, nil, http.StatusNotFound)
+}
+
+func RenderBadRequestResponse(w http.ResponseWriter) {
+	RenderResponse(w, nil, http.StatusBadRequest)
 }

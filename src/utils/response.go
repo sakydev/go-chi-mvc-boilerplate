8f0dc8791749795ecc5fe07cb3298656f4de2e06
Saqib Razzaq <saqib.razzaq@plana.earth>
2024-04-21 16:25:40 +0200

    Better error handling in response

--- src/utils/response.go
+++ src/utils/response.go
@@ -2,16 +2,30 @@ package utils
 
 import (
 	"encoding/json"
+	"errors"
 	"net/http"
 )
 
 const defaultContentType = "application/json"
 
-func RenderResponse(w http.ResponseWriter, data interface{}, status int) {
+type Response struct {
+	Error   string      `json:"error,omitempty"`
+	Content interface{} `json:"content"`
+}
+
+func RenderResponse(w http.ResponseWriter, content interface{}, status int, error error) {
 	w.Header().Set("Content-Type", defaultContentType)
 	w.WriteHeader(status)
 
-	jsonData, err := json.Marshal(data)
+	errorMessage := ""
+	if error != nil {
+		errorMessage = error.Error()
+	}
+
+	jsonData, err := json.Marshal(Response{
+		Error:   errorMessage,
+		Content: content,
+	})
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -20,10 +34,14 @@ func RenderResponse(w http.ResponseWriter, data interface{}, status int) {
 	w.Write(jsonData)
 }
 
-func RenderNotFoundResponse(w http.ResponseWriter) {
-	RenderResponse(w, nil, http.StatusNotFound)
+func RenderNotFoundResponse(w http.ResponseWriter, message string) {
+	RenderResponse(w, nil, http.StatusNotFound, errors.New(message))
 }
 
-func RenderBadRequestResponse(w http.ResponseWriter) {
-	RenderResponse(w, nil, http.StatusBadRequest)
+func RenderBadRequestResponse(w http.ResponseWriter, message string) {
+	RenderResponse(w, nil, http.StatusBadRequest, errors.New(message))
+}
+
+func RenderInternalServerResponse(w http.ResponseWriter, message string) {
+	RenderResponse(w, nil, http.StatusInternalServerError, errors.New(message))
 }

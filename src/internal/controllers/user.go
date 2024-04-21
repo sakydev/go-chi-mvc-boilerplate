ef00cb3c4b00a3925d707e011d6a1c6d6e54a457
Saqib Razzaq <saqib.razzaq@plana.earth>
2024-04-21 16:26:03 +0200

    Format error correctly

--- src/internal/controllers/user.go
+++ src/internal/controllers/user.go
@@ -2,8 +2,10 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 	"github.com/go-chi/chi/v5"
 	"github.com/samber/do"
+	"go-chi-mvc-boilerplate/src/internal/types"
 	"go-chi-mvc-boilerplate/src/utils"
 	"net/http"
 
@@ -11,38 +13,43 @@ import (
 )
 
 type UserController struct {
-	ctx         context.Context
 	userService *services.UserService
 }
 
 func NewUserController(i *do.Injector, ctx context.Context) *UserController {
 	return &UserController{
-		ctx:         ctx,
 		userService: do.MustInvoke[*services.UserService](i),
 	}
 }
 
 func (uc *UserController) ListUsers(w http.ResponseWriter, r *http.Request) {
-	users, err := uc.userService.List(uc.ctx)
+	users, err := uc.userService.List(r.Context())
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 
-	utils.RenderResponse(w, users, http.StatusOK)
+	utils.RenderResponse(w, users, http.StatusOK, nil)
 }
 
 func (uc *UserController) GetUser(w http.ResponseWriter, r *http.Request) {
 	email := chi.URLParam(r, "email")
 	if len(email) < 2 {
-		utils.RenderNotFoundResponse(w)
+		utils.RenderNotFoundResponse(w, "user not found")
 
 		return
 	}
 
-	users, err := uc.userService.GetByEmail(uc.ctx, email)
+	users, err := uc.userService.GetByEmail(r.Context(), email)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		utils.RenderInternalServerResponse(w, fmt.Sprintf("error fetching user: %w", err))
 	}
 
-	utils.RenderResponse(w, users, http.StatusOK)
+	utils.RenderResponse(w, users, http.StatusOK, nil)
+}
+
+func (uc *UserController) CreateUser(w http.ResponseWriter, r *http.Request) {
+	requestContent := r.Context().Value("validated").(types.CreateUserRequest)
+
+	createdUser, err := uc.userService.Create(r.Context(), requestContent)
+	utils.RenderResponse(w, createdUser, http.StatusOK, err)
 }
